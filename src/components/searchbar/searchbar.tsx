107aea70d0df0614a2faf12db8154fb3023e55b1
Hassaan11 <saad117218@gmail.com>
2023-09-29 04:33:53 +0500

    Added search functionality

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -3,26 +3,25 @@ import { Input } from "antd";
 
 import "./searchbar.css";
 
-const Searchbar = () => {
+const Searchbar = ({ onSearchChange }: any) => {
   return (
-    <div>
-      <div className="inputdiv">
-        <Input
-          className="w-100"
-          size="large"
-          placeholder="Serach by name, edu, exp or #tag"
-          prefix={
-            <SearchOutlined
-              style={{
-                color: "#B0BABF",
-                marginRight: "10px",
-                fontSize: "20px",
-              }}
-            />
-          }
-        />
-        <InfoCircleOutlined className="infoIcon" />
-      </div>
+    <div className="inputdiv">
+      <Input
+        className="w-100"
+        size="large"
+        placeholder="Serach by name, edu, exp or #tag"
+        onChange={onSearchChange}
+        prefix={
+          <SearchOutlined
+            style={{
+              color: "#B0BABF",
+              marginRight: "10px",
+              fontSize: "20px",
+            }}
+          />
+        }
+      />
+      <InfoCircleOutlined className="infoIcon" />
     </div>
   );
 };
