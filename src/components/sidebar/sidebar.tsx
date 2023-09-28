0998676b968b7fa9cd9561bd1bcf093745cd5247
Hassaan11 <saad117218@gmail.com>
2023-09-29 04:09:47 +0500

    highlight home option of sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -11,15 +11,16 @@ import {
   LeftOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
+import { useState } from "react";
 
 import "./sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="d-flex flex-column justify-content-between sidebar bg-white">
-      <div className="d-flex flex-column">
+      <div className="d-flex flex-column align-items-center">
         <MenuOutlined className="icon" />
-        <HomeOutlined className="icon" />
+        <HomeOutlined className="icon active" />
         <UsergroupAddOutlined className="icon" />
         <CalendarOutlined className="icon" />
         <ShareAltOutlined className="icon" />
