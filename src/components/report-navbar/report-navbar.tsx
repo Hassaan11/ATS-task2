0d1084b178ad20b35d5f2e6b2ad329c0cb339b0d
Hassaan11 <saad117218@gmail.com>
2023-09-29 08:38:30 +0500

    Added navbar for reports page

---
+++ src/components/report-navbar/report-navbar.tsx
@@ -0,0 +1,47 @@
+import { DownOutlined } from "@ant-design/icons";
+import { DatePicker, DatePickerProps } from "antd";
+import dayjs from "dayjs";
+import moment from "moment";
+
+import "./report-navbar.css";
+
+const ReportNavbar = ({
+  setStartDate,
+  setEndDate,
+  startDate,
+  endDate,
+}: any) => {
+  const { RangePicker } = DatePicker;
+
+  const customFormat: DatePickerProps["format"] = (value: any) =>
+    `${moment(value?.$d).format("DD MMM YYYY")}`;
+
+  return (
+    <div className="d-flex justify-content-between align-items-center flex-wrap p-4 report-navbar">
+      <h3 className="title">Opportunity Overview</h3>
+      <div className="d-flex align-items-center">
+        <h5>
+          London Internship Program{" "}
+          <DownOutlined style={{ fontSize: "10px", marginLeft: "10px" }} />
+        </h5>
+        <div className="datePicker">
+          <RangePicker
+            style={{
+              backgroundColor: "#F0F3FB",
+              border: "none",
+              borderRadius: "20px",
+            }}
+            defaultValue={[dayjs("01/01/2023"), dayjs("07/31/2023")]}
+            format={customFormat}
+            onChange={(e: any) => {
+              setStartDate(e[0]?.$d);
+              setEndDate(e[1]?.$d);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReportNavbar;
