647893745e04b537bf8dd4656718714dc7aa91fd
Hassaan11 <saad117218@gmail.com>
2023-09-29 08:39:10 +0500

    Setup component to display line chart

---
+++ src/components/line-chart/line-chart.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
+);
+
+const LineChart = ({ data }: any) => {
+  return (
+    <Line
+      options={{
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top" as const,
+          },
+          title: {
+            display: true,
+            text: "Chart.js Line Chart",
+          },
+        },
+      }}
+      data={data}
+      updateMode="active"
+    />
+  );
+};
+
+export default LineChart;
