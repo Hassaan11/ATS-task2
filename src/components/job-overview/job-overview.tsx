2268c4acb16013e3737cee2ee56847a6ca21a6a9
Hassaan11 <saad117218@gmail.com>
2023-10-01 01:27:43 +0500

    Setup component to display overview of comapny

--- src/components/job-overview/job-overview.tsx
+++ src/components/job-overview/job-overview.tsx
@@ -4,41 +4,25 @@ import { Dropdown, Space, Progress } from "antd";
 
 import Box from "../box/box";
 import LineChart from "../line-chart/line-chart";
+
 import "./job-overview.css";
+import { Data1Item, ReportProps } from "../../types/types";
+import { dropdownData } from "../../constants";
+
+type JobOverviewProps = {
+  title?: string;
+  reportData: ReportProps;
+  activeDropdown: string;
+  setActiveDropdown: any;
+};
 
 const JobOverview = ({
-  title,
+  title = "",
   reportData,
   activeDropdown,
   setActiveDropdown,
-}: any) => {
-  const items: MenuProps["items"] = [
-    {
-      label: "Applied",
-      key: "0",
-    },
-    {
-      label: "Recommended",
-      key: "1",
-    },
-    {
-      label: "Interview",
-      key: "2",
-    },
-    {
-      label: "Offer",
-      key: "3",
-    },
-    {
-      label: "Hired",
-      key: "4",
-    },
-    {
-      label: "Rejected",
-      key: "5",
-    },
-  ];
-
+}: JobOverviewProps) => {
+  const items: MenuProps["items"] = dropdownData;
   const handleMenuClick: MenuProps["onClick"] = (e: any) => {
     setActiveDropdown(e?.domEvent.target.innerText);
   };
@@ -49,15 +33,16 @@ const JobOverview = ({
   };
 
   return (
-    <div className="job-overview">
+    <div className="job-overview mb-5">
       <h6 className="title mb-3">{title}</h6>
-      <div className="d-flex">
+      <div className="job-overview2">
         <div className="c1 d-flex flex-column">
-          <div className="d-flex mb-3">
-            {reportData?.data?.map((d: any, index: number) => {
+          <div className="d-flex mb-3 boxesDiv">
+            {reportData?.data1?.map((d: Data1Item, index: number) => {
               if (index < 3) {
                 return (
                   <Box
+                    key={index}
                     name={d?.name}
                     count={d?.count}
                     prevCount={d?.prevCount}
@@ -65,7 +50,7 @@ const JobOverview = ({
                     percentage={d?.percentage}
                   />
                 );
-              } else return <></>;
+              }
             })}
           </div>
           <div
@@ -111,13 +96,15 @@ const JobOverview = ({
                 </span>
               </div>
             </div>
-            <LineChart data={reportData?.graphData} />
+            <div style={{ height: "300px", width: "100%" }}>
+              <LineChart data={reportData?.graphData} />
+            </div>
           </div>
         </div>
         <div className="c2 d-flex flex-column">
           <div className="p-4 first">
-            <h5>Total Candidate Flow</h5>
-
+            <h5 className="mb-3">Total Candidate Flow</h5>
+            <h6 className="mb-4">4,567</h6>
             <Progress
               strokeLinecap="butt"
               percent={75}
@@ -125,6 +112,47 @@ const JobOverview = ({
               trailColor="#FFE0E0"
               showInfo={false}
             />
+            <div className="d-flex progresstext">
+              <span className="w-75">Qualified 78%</span>
+              <span className="w-25">Disqualified 22%</span>
+            </div>
+            <div className="mt-4 d-flex justify-content-between align-items-center details mb-2 w-100 heading">
+              <span className="w-50">Stage</span>
+              <div className="w-50 d-flex justify-content-between right">
+                <span className="w-50">Qualified</span>
+                <span className="w-50">Disqualified</span>
+              </div>
+            </div>
+            {reportData?.data1?.map((d: Data1Item, index: number) => (
+              <div
+                key={index}
+                className="d-flex justify-content-between align-items-center details mb-2 w-100"
+              >
+                <span className="w-50">{d?.name}</span>
+                <div className="w-50 d-flex justify-content-between right">
+                  <span className="w-50">{d?.count}</span>
+                  <span className="w-50">{d?.disqualified}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="d-flex mt-3">
+            {reportData?.data1?.map((d: Data1Item, index: number) => {
+              if (index > 2) {
+                return (
+                  <div className="boxDiv1" key={index}>
+                    <Box
+                      changeDesign={true}
+                      name={d?.name}
+                      count={d?.count}
+                      prevCount={d?.prevCount}
+                      rise={d?.rise}
+                      percentage={d?.percentage}
+                    />
+                  </div>
+                );
+              } else return <div key={index}></div>;
+            })}
           </div>
         </div>
       </div>
