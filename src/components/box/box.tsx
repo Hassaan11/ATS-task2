730f959dd5a7a463d8012f337d951c869f1b58d5
Hassaan11 <saad117218@gmail.com>
2023-10-01 01:25:16 +0500

    Setup component to display applicants report

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,18 +1,27 @@
 import { FallOutlined, RiseOutlined } from "@ant-design/icons";
+import { BoxProps } from "../../types/types";
 
-type BoxProps = {
-  name: string;
-  count: string;
-  prevCount: string;
-  rise: boolean;
-  percentage: string;
-};
-
-const Box = ({ name, count, prevCount, rise, percentage }: any) => {
+const Box = ({
+  name,
+  count,
+  prevCount,
+  rise,
+  percentage,
+  changeDesign = false,
+}: BoxProps) => {
   return (
     <div className="p-4 box">
-      <h5>{name}</h5>
-      <h6>{count}</h6>
+      {changeDesign ? (
+        <div className="d-flex justify-content-between align-items-center">
+          <h5>{name}</h5>
+          <h6>{count}</h6>
+        </div>
+      ) : (
+        <>
+          <h5>{name}</h5>
+          <h6>{count}</h6>
+        </>
+      )}
       <span className="prevPeriod mb-2">Previous Period</span>
       <div className="d-flex align-items-center">
         <span className="prevPeriod">{prevCount}</span>
